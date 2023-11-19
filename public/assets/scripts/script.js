2eca0a66e0e9f910733a1050bfbe75a0398900fa
Jess-0608 <hyej356@gmail.com>
2023-11-19 00:03:17 -0500

    feat(interaction): added interactions using javascript

---
+++ public/assets/scripts/script.js
@@ -0,0 +1,26 @@
+let currentSlideIndex = 0;
+
+function moveSlide(direction) {
+  const slides = document.querySelectorAll('.blog-item');
+  const totalSlides = slides.length;
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  const blogContent = document.querySelector('.blog-content');
+  
+  currentSlideIndex += direction;
+  
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = 0;
+  }
+
+  if (currentSlideIndex >= totalSlides) {
+    currentSlideIndex = totalSlides - 1;
+  }
+
+  const transformValue = -(currentSlideIndex * slideWidth);
+  blogContent.style.transform = `translateX(${transformValue}px)`;
+}
+
+document.getElementById('subscription-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Subscription form submitted!'); 
+});
